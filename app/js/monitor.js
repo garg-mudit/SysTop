dc9653573d0483ece2914e358368b98e8b2f6d8f
Mudit Garg <itzmuditgarg@gmail.com>
2020-09-06 14:20:09 +0530

    Added more system stats that will be updated every 2 seconds

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,33 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+let cpuOverload = 10;
+
+//Run every 2 seconds
+setInterval(() => {
+  //CPU Usage
+  cpu.usage().then((info) => {
+    document.getElementById('cpu-usage').innerHTML = info + '%';
+
+    document.getElementById('cpu-progress').style.width = info + '%';
+
+    //Make progressbar red if overload
+    if (info > cpuOverload) {
+      document.getElementById('cpu-progress').style.background = 'red';
+    } else {
+      document.getElementById('cpu-progress').style.background = '#30c88b';
+    }
+  });
+
+  //CPU Free
+  cpu.free().then((info) => {
+    document.getElementById('cpu-free').innerHTML = info + '%';
+  });
+
+  //Uptime
+  document.getElementById('sys-uptime').innerHTML = secondsToDhms(os.uptime());
+}, 2000);
+
 //Set model
 document.getElementById('cpu-model').innerHTML = cpu.model();
 
@@ -17,3 +44,13 @@ document.getElementById('os').innerHTML = `${os.type()} ${os.arch()}`;
 mem.info().then((info) => {
   document.getElementById('mem-total').innerHTML = `${info.totalMemMb}Mb`;
 });
+
+//Show days, hours, mins, sec
+function secondsToDhms(seconds) {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
